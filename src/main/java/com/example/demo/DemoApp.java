890cf7bfc2fd369162e329f3b69728a3ec304a14
unknown <jadhavajit2024@gmail.com>
2024-07-05 06:43:37 +0530

    My first test app

---
+++ src/main/java/com/example/demo/DemoApp.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is just");
+	}
+
+}
